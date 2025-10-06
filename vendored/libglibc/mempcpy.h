9c6fe030f11906784a8d867e6ce8a6eaa8eb3a87
Jim Garlick <garlick.jim@gmail.com>
2025-10-06 15:18:58 -0700

    libmissing: add mempcpy, argz
    Problem: liboptparse requires argz and mempcpy, which aren't
    available on macos.
    
    Pull the argz, mempcpy source from flux-core.
    Populate AC_REPLACE_FUNCS.
    
    Since in this project we're trying to isolate vendored code,
    put the glibc bits in /vendored/libglibc and create stubs that
    include those files in libmissing.
    
    As a reminder, libmissing works as follows:
    - functions in AC_REPLACE_FUNCS are checked
    - config.h contains HAVE_name defines
    - objects corresponding to missing/broken functions are added
      to LTLIBOBJS.  For example, if argz_add() is missing,
      LTLIBOBJS includes argz_add.lo
    - the objects in LTLIBOBJS are added to libmissing
    - the sources for those objects are found in libmissing

---
+++ vendored/libglibc/mempcpy.h
@@ -0,0 +1,25 @@
+/* Copy memory to memory until the specified number of bytes
+   has been copied, return pointer to following byte.
+   Overlap is NOT handled correctly.
+   Copyright (C) 1991-2025 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Torbjorn Granlund (tege@sics.se).
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <sys/types.h>
+#include <string.h>
+
+void *mempcpy (void *dest, const void *src, size_t len);

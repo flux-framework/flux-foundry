8bbab7863141669998e89233f15f20036cb0bfea
Jim Garlick <garlick.jim@gmail.com>
2025-10-07 14:52:23 -0700

    allow in-tree inclusion of <flux/idset.h>
    Problem: headers for foundry classes like taskmap will need to
    include idset.h via the installed path, but that won't work in-tree.
    
    Create a idset.h stub in src/include/flux so that the installed
    path can work in-tree.

---
+++ src/include/flux/idset.h
@@ -0,0 +1,18 @@
+/************************************************************\
+ * Copyright 2025 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+// Allow in-tree sources to #include <flux/idset.h>
+
+#ifndef FAKE_FLUX_IDSET_H
+#define FAKE_FLUX_IDSET_H
+
+#include "src/libidset/idset.h"
+
+#endif // !FAKE_FLUX_IDSET_H
